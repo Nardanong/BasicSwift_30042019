25cc13106e70a208f4cded304fa5167311e9ab80
akapan <akapan.r@hitachi-tstv.com>
2019-04-30 11:47:43 +0700

    Variable And Constant xCode

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+//Example Playground
+//var str = "Hello, playground"
+
+//Var and constructor
+//Implicit and Variable //คือการประกาศตัวแปรโดยไม่ต้องกำหนด - Data Type ให้กับตัวแปร
+var nameString = "Nardanong.s"
+var numberInt = 100
+var numberDou = 12.50
+var statusBol = true
+
+//Explicit and Variable //คือการประกาศตัวแปรโดยต้องกำหนด - Data Type ให้กับตัวแปรเสมอ
+var surname: String = "Sukbua"
+var numberInt1: Int = 150
+var numnerDou1: Double = 10.50
+var statusBol1: Bool = false
+
+//Constant
+let mynickname: String = "Pu"
+
+//Test Variable
+nameString = "Nardanong.s1234"
+//mynickname = "Pu1234" // Cannot Compile Error Because Data Type is let
+
+
+
+
+
+
+
+
+
