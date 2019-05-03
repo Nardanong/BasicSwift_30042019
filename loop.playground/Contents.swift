944ea726fb9d261dbf03fd6650447e8e865a0a4d
akapan <akapan.r@hitachi-tstv.com>
2019-05-03 13:21:56 +0700

    loop

---
+++ loop.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var number = [1,2,3,4,5]
+for mynumber in number{
+    print("Loop number ==> \(mynumber)")
+}
+
+var count: Int = 0
+while count<=10 {
+    print("loop of count ==> \(count)")
+    count+=1
+}
